4e2daba30aff2d4c201123a771a57ea2e5247e0c
DeadDreik <65324697+DeadDreik@users.noreply.github.com>
2024-06-01 16:03:50 +0300

    Add files via upload

---
+++ main — копия.py
@@ -0,0 +1,28 @@
+# Задача «Длина слова»
+a = "heelo teacher"
+print(len(a))
+# Задача «Суммы и разности»
+first = 14
+second = 16
+summa = first + second
+diff = first - second
+print(summa, second)
+
+# Задача «Среднее арифметическое»
+first = 7
+second = 14
+third = 19
+meam = (first + second + third) // 3
+print(meam)
+
+# Задача «Простые строчки»
+first_string = "Вторник"
+second_string = "Понедельник"
+print(second_string, ", ", first_string )
+
+# Задача «Сложная формула»
+a = 3
+b = 7
+c = 2
+f = ((a * b) + (a * c)**3) // 2
+print(f)
